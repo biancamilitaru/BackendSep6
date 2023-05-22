90661a92896274566bee68993240c4a180d5d160
kyratolnai11 <266375@viauc.dk>
2023-05-22 08:42:52 +0200

    New deployment

--- help/Program.cs
+++ help/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+        policy  =>
+        {
+            policy.WithOrigins("https://localhost:7186", "http://localhost:5006", "https://backend4hell.azurewebsites.net/api/", "https://movieforu.azurewebsites.net/").AllowAnyHeader().AllowAnyMethod();
+        });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
